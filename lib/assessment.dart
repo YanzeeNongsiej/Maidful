84f40d141fbed393db1ccb63269039b0160f7bda
Iaineh M Nongsiej <yanzeenongsiej@gmail.com>
2024-10-15 17:23:07 +0530

    oct 15 Assessment

--- lib/assessment.dart
+++ lib/assessment.dart
@@ -1,9 +1,19 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ibitf_app/singleton.dart';
 import 'package:ibitf_app/xmlhandle.dart';
+import 'dart:math';
+import 'dart:async';
 
 String? skill;
+int currentQuestionIndex = 0;
+int timerDuration = 180; // 3 minutes in seconds
+Timer? timer;
+ValueNotifier<int>? timerNotifier;
+List<int> random = [];
+List<bool> ans = [];
 
 class Assessment extends StatefulWidget {
   Assessment(String? s) {
@@ -15,108 +25,368 @@ class Assessment extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<Assessment> {
-  @override
   XMLHandler _xmlHandler = XMLHandler();
   GlobalVariables gv = GlobalVariables();
-  List<Map<String, dynamic>>? questions;
-  String? question;
-  List<String>? options;
-  String? answer;
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _xmlHandler.loadStrings(gv.selected).then((a) {
-      loadAllQuestions();
-      setState(() {});
-    });
-  }
-
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        foregroundColor: Colors.black,
-        surfaceTintColor: Colors.red,
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(skill.toString(), style: const TextStyle(fontSize: 20)),
-          ],
-        ),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.blue, // Set your desired background color
-          borderRadius: BorderRadius.circular(12),
-        ),
-        width: MediaQuery.of(context).size.width,
-        child: Column(children: [
-          Card(
-            color: Colors.blueAccent[100],
-            elevation: 10,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('ello'),
-            ),
-          ),
-        ]),
-      ),
-    );
+  void initState() {
+    super.initState();
+    timerNotifier = ValueNotifier<int>(timerDuration);
+    ans = [false, false, false, false, false];
   }
 
-  void loadAllQuestions() async {
+  Future<List<Map<String, dynamic>>> loadAllQuestions() async {
     final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
     // Get the specific skill document
     QuerySnapshot snapshot = await _firestore
         .collection('skills')
-        .where(gv.selected, isEqualTo: skill.toString()) // Use the passed skill
-        .get();
-    DocumentSnapshot skillDoc = await _firestore
-        .collection('skills')
-        .doc(snapshot.docs.first.id) // Use the specific skill ID
+        .where(gv.selected, isEqualTo: skill.toString())
         .get();
 
-    if (skillDoc.exists) {
-      // Now query the Questions subcollection
-      QuerySnapshot questionsSnapshot = await _firestore
-          .collection('skills')
-          .doc(snapshot.docs.first.id) // Use the specific skill ID
-          .collection('questions')
-          .get();
-
-      questions = [];
-
-      for (var doc in questionsSnapshot.docs) {
-        String qid = doc.id; // Get the question ID (e.g., Q1, Q2)
-        String question = doc['English'];
-        List<String> options = List<String>.from(doc['EnglishOptions']);
-        String answer = doc['Ans']; // Get the answer (e.g., "0")
-
-        questions!.add({
-          'id': qid, // Include question ID
-          'question': question,
-          'options': options,
-          'answer': answer, // Include the answer
-        });
-      }
-
-      // Print or return the questions as needed
-      for (var question in questions!) {
-        print('ID: ${question['id']}');
-        print('Question: ${question['question']}');
-        print('Options: ${question['options']}');
-        print('Answer: ${question['answer']}');
-      }
-    } else {
-      print('No data found for the specified skill.');
+    if (snapshot.docs.isEmpty) {
+      throw Exception('No skill found');
     }
+
+    DocumentSnapshot skillDoc =
+        await _firestore.collection('skills').doc(snapshot.docs.first.id).get();
+
+    if (!skillDoc.exists) {
+      throw Exception('No data found for the specified skill.');
+    }
+
+    // Query the Questions subcollection
+    QuerySnapshot questionsSnapshot = await _firestore
+        .collection('skills')
+        .doc(snapshot.docs.first.id)
+        .collection('questions')
+        .get();
+
+    List<Map<String, dynamic>> questions = [];
+
+    for (var doc in questionsSnapshot.docs) {
+      String qid = doc.id;
+      String question = doc[gv.selected];
+      List<String> options = List<String>.from(doc['${gv.selected}Options']);
+      String answer = doc['Ans'];
+
+      questions.add({
+        'id': qid,
+        'question': question,
+        'options': options,
+        'answer': answer,
+      });
+    }
+    //generate random numbers for questions
+    if (random.isEmpty) {
+      random = generateRandomIntegers(5, 0, questions.length - 1);
+    }
+
+    startTimer();
+    return questions;
+  }
+
+  void processAnswers() {}
+  List<int> generateRandomIntegers(int count, int min, int max) {
+    final Random random = Random();
+
+    // Generate a set of unique random integers
+    final Set<int> randomIntegers = Set<int>.from(
+        List.generate(count, (index) => min + random.nextInt(max - min + 1)));
+
+    // If there are not enough unique integers, fill until the required count
+    while (randomIntegers.length < count) {
+      randomIntegers.add(min + random.nextInt(max - min + 1));
+    }
+
+    return randomIntegers.toList();
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (timerNotifier!.value > 0) {
+        timerNotifier!.value--;
+      } else {
+        timer.cancel();
+        showTimeUpDialog();
+      }
+    });
+  }
+
+  void showTimeUpDialog() {
+    if (mounted) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Time's Up!"),
+            content: Text("You've run out of time for this assessment."),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  // Optionally, navigate back or handle the end of assessment
+                },
+                child: Text("OK"),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel(); // Cancel the timer when the widget is disposed
+    super.dispose();
+    currentQuestionIndex = 0;
+    selOpt = null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          foregroundColor: Colors.black,
+          surfaceTintColor: Colors.red,
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: ValueListenableBuilder<int>(
+                  valueListenable: timerNotifier!,
+                  builder: (context, remainingTime, child) {
+                    return Text(
+                      "Time Left: ${remainingTime ~/ 60}:${(remainingTime % 60).toString().padLeft(2, '0')}",
+                      style: TextStyle(fontSize: 16),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+        body: FutureBuilder<List<Map<String, dynamic>>>(
+          future: loadAllQuestions(), // Call your fetch function here
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(child: Text('No questions available.'));
+            }
+
+            // Use the loaded questions to display in the UI
+            List<Map<String, dynamic>> questions = snapshot.data!;
+            Map<String, dynamic> currentQuestion =
+                questions[random[currentQuestionIndex]];
+            return Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+                color: Colors.blueAccent[100],
+              ),
+
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Text('ID: ${questions[0]['id']}'),
+
+                      Text(
+                        'Question: ${questions[random[currentQuestionIndex]]['question']}',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      MyOptions(currentQuestion),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (selOpt != null) {
+                              setState(() {
+                                // Check if there are more questions
+                                if (questions[random[currentQuestionIndex]]
+                                            ['answer']
+                                        .toString() ==
+                                    selOpt.toString()) {
+                                  ans[currentQuestionIndex] = true;
+                                } else {
+                                  ans[currentQuestionIndex] = false;
+                                }
+                                if (currentQuestionIndex < random.length - 1) {
+                                  currentQuestionIndex++;
+                                  selOpt = null;
+                                  // Reset the selected option
+                                } else {
+                                  // Handle end of questions (e.g., show results)
+                                  // For now, just show a dialog
+                                  int correct = 0;
+                                  double percentage;
+                                  for (int i = 0; i < ans.length; i++) {
+                                    if (ans[i] == true) {
+                                      correct++;
+                                    }
+                                  }
+
+                                  percentage = (correct / random.length) * 100;
+                                  Navigator.pop(context);
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: Text(
+                                            "You've completed the assessment"),
+                                        content: Text("Score:$percentage%"),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: Text("OK"),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                }
+                              });
+                            }
+                          },
+                          child: Text('Submit'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // child: ListView.builder(
+              //   itemCount: questions.length,
+              //   itemBuilder: (context, index) {
+              //     return Card(
+              //       color: Colors.blueAccent[100],
+              //       elevation: 10,
+              //       child: Padding(
+              //         padding: const EdgeInsets.all(8.0),
+              //         child: Column(
+              //           crossAxisAlignment: CrossAxisAlignment.start,
+              //           children: [
+              //             Text('ID: ${questions[index]['id']}'),
+              //             Text('Question: ${questions[index]['question']}'),
+              // You can also display options and answer if needed
+              // ],
+              //           ),
+              //         ),
+              //       );
+              //     },
+              //   ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Map<String, dynamic>? question;
+String? selOpt;
+
+// ignore: must_be_immutable
+class MyOptions extends StatefulWidget {
+  MyOptions(Map<String, dynamic> currentQuestion, {super.key}) {
+    question = currentQuestion;
+  }
+
+  @override
+  State<MyOptions> createState() => _MyOptionsState();
+}
+
+class _MyOptionsState extends State<MyOptions> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          color: Colors.white,
+          elevation: 5,
+          child: ListTile(
+            title: Text(question!['options'][0]),
+            leading: Radio<String>(
+              value: '0',
+              groupValue: selOpt,
+              onChanged: (value) {
+                setState(() {
+                  selOpt = value;
+                });
+              },
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Card(
+          color: Colors.white,
+          elevation: 5,
+          child: ListTile(
+            title: Text(question!['options'][1]),
+            leading: Radio<String>(
+              value: '1',
+              groupValue: selOpt,
+              onChanged: (value) {
+                setState(() {
+                  selOpt = value;
+                });
+              },
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Card(
+          color: Colors.white,
+          elevation: 5,
+          child: ListTile(
+            title: Text(question!['options'][2]),
+            leading: Radio<String>(
+              value: '2',
+              groupValue: selOpt,
+              onChanged: (value) {
+                setState(() {
+                  selOpt = value;
+                });
+              },
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        Card(
+          color: Colors.white,
+          elevation: 5,
+          child: ListTile(
+            title: Text(question!['options'][3]),
+            leading: Radio<String>(
+              value: '3',
+              groupValue: selOpt,
+              onChanged: (value) {
+                setState(() {
+                  selOpt = value;
+                });
+              },
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+      ],
+    );
   }
 }
