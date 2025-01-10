9da282e0f527560f93baa11e1f8e81d3230bd23e
Skhem Sohphoh <skhemsohphoh@gmail.com>
2025-01-10 15:33:39 +0530

    jan 10 2025 Skhem Search feature

--- lib/landinghome.dart
+++ lib/landinghome.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +11,7 @@ import 'package:ibitf_app/singleton.dart';
 
 class LandingHomePage extends StatefulWidget {
   final String? uname;
+  // Callback function passed from NestedTabBar
 
   const LandingHomePage({super.key, @required this.uname});
 
@@ -16,7 +19,13 @@ class LandingHomePage extends StatefulWidget {
   State<LandingHomePage> createState() => _LandingHomePageState();
 }
 
+late Future<QuerySnapshot> aqs, searchqs;
+int searchCriteria = 1; //1=Address,2=Name,3=Service
+SearchController? sCont;
+
 class _LandingHomePageState extends State<LandingHomePage> {
+  String searchText = "Search by Address...";
+
   @override
   void initState() {
     // TODO: implement initState
@@ -45,7 +54,110 @@ class _LandingHomePageState extends State<LandingHomePage> {
                   ),
                 ),
               ),
-              const NestedTabBar(),
+
+//search button
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SearchAnchor(
+                      builder: (BuildContext context, SearchController sCont) {
+                    return SizedBox(
+                      height: MediaQuery.of(context).size.height / 20,
+                      child: SearchBar(
+                        controller: sCont,
+                        hintText: searchText,
+                        padding: const WidgetStatePropertyAll<EdgeInsets>(
+                            EdgeInsets.only(left: 8.0)),
+                        onTap: () {
+                          // controller.openView();
+                        },
+                        onChanged: (_) {
+                          updateServiceList(sCont.text);
+                          // controller.openView();
+                        },
+                        leading: const Icon(Icons.search),
+                        trailing: <Widget>[
+                          PopupMenuButton(
+                            icon: const Icon(
+                              Icons.list,
+                            ),
+                            // onSelected: handleClick,
+                            itemBuilder: (BuildContext context) {
+                              return [
+                                PopupMenuItem(
+                                  value: "Logout",
+                                  child: const Text("Search by Address"),
+                                  onTap: () {
+                                    setState(() {
+                                      searchText = "Search by Address...";
+                                      searchCriteria = 1;
+                                    });
+                                  },
+                                ),
+                                PopupMenuItem(
+                                  value: "Settings",
+                                  child: Text("Search by Name"),
+                                  onTap: () {
+                                    setState(() {
+                                      searchText = "Search by Name...";
+                                      searchCriteria = 2;
+                                    });
+                                  },
+                                ),
+                                PopupMenuItem(
+                                  value: "Contact",
+                                  child: Text("Search by Service"),
+                                  onTap: () {
+                                    setState(() {
+                                      searchText = "Search by Service...";
+                                      searchCriteria = 3;
+                                    });
+                                  },
+                                ),
+                              ];
+                            },
+                          ),
+                        ],
+                        //old
+                        // <Widget>[
+                        //   Tooltip(
+                        //     message: 'Search options',
+                        //     child: IconButton(
+                        //       isSelected: isDark,
+                        //       onPressed: () {
+                        //         print("Search options");
+
+                        //         // setState(() {
+                        //         //   isDark = !isDark;
+                        //         // });
+                        //       },
+                        //       icon: const Icon(
+                        //         Icons.list,
+                        //       ),
+                        //       // selectedIcon:
+                        //       //     const Icon(Icons.brightness_2_outlined),
+                        //     ),
+                        //   )
+                        // ],
+                      ),
+                    );
+                  }, suggestionsBuilder:
+                          (BuildContext context, SearchController controller) {
+                    return List<ListTile>.generate(5, (int index) {
+                      final String item = 'item $index';
+                      return ListTile(
+                        title: Text(item),
+                        onTap: () {
+                          setState(() {
+                            controller.closeView(item);
+                          });
+                        },
+                      );
+                    });
+                  })),
+
+              NestedTabBar(refreshCallback: () {
+                setState(() {}); // Call setState of NestedTabBar
+              }),
 
               // const DefaultTabController(
               //   length: 2,
@@ -150,11 +262,154 @@ class _LandingHomePageState extends State<LandingHomePage> {
           );
         });
   }
+
+  void updateServiceList(String searchVal) {
+    aqs.then((QuerySnapshot s) {
+      List<QueryDocumentSnapshot> filteredRes = [];
+      String searchBy = 'address';
+      if (searchCriteria == 1) {
+        searchBy = 'address';
+      } else if (searchCriteria == 2) {
+        searchBy = 'name';
+      } else {
+        searchBy = 'services';
+      }
+      var futures;
+      if (searchCriteria == 1 || searchCriteria == 2) {
+        // Loop through each document in the initial QuerySnapshot
+        futures = s.docs.map((i) {
+          return fetchUserData(i.get('userid')).then((QuerySnapshot temp) {
+            // Filter the documents based on the search value
+            List<QueryDocumentSnapshot> userDocs = temp.docs.where((doc) {
+              return doc[searchBy]
+                  .toString()
+                  .toLowerCase()
+                  .startsWith(searchVal.toLowerCase());
+            }).toList();
+            // Add filtered docs to the result listM
+            filteredRes.addAll(userDocs);
+          });
+        }).toList();
+        Future<QuerySnapshot> usr;
+        Future.wait(futures).then((_) {
+          usr = Future.value(MockQuerySnapshot(filteredRes));
+          // Update state with filtered results
+          usr.then((data) {
+            List<String> usrUserIds = [];
+            for (var i in data.docs) {
+              usrUserIds.add(i.get('userid'));
+            }
+            List<QueryDocumentSnapshot> filteredSearchQs = [];
+
+            aqs.then((a) {
+              for (var doc in a.docs) {
+                if (usrUserIds.contains(doc.get('userid'))) {
+                  filteredSearchQs.add(doc);
+                }
+              }
+            });
+            searchqs = Future.value(MockQuerySnapshot(filteredSearchQs));
+            print("filtered");
+            setState(() {
+              searchqs.then((a) {});
+            });
+          });
+
+          // Step 2: Filter searchqs based on common 'userid' with usr
+
+          // print(searchqs); // For debugging: print the filtered results
+        }).catchError((e) {
+          print('Error in fetchUserData: $e');
+        });
+      } else {
+        // return aqs.then((QuerySnapshot temp) {
+        // Filter the documents based on the search value
+        List<String> usrServices = [];
+        List<QueryDocumentSnapshot> userDocs = s.docs.where((doc) {
+          bool flg = false;
+          List<dynamic> size = doc[searchBy];
+          for (int j = 0; j < size.length; j++) {
+            flg = false;
+            if (doc[searchBy][j]
+                .toString()
+                .toLowerCase()
+                .trim()
+                .startsWith(searchVal.toLowerCase().trim())) {
+              usrServices.add(doc[searchBy][j]);
+              flg = true;
+              break;
+            }
+          }
+          return flg;
+          // return doc[searchBy]
+          //     .toString()
+          //     .toLowerCase()
+          //     .startsWith(searchVal.toLowerCase());
+        }).toList();
+        // Add filtered docs to the result listM
+        filteredRes.addAll(userDocs);
+        Future<QuerySnapshot> finalres =
+            Future.value(MockQuerySnapshot(filteredRes));
+        finalres.then((data) {
+          for (var i in data.docs) {
+            // usrServices.add(i.get(searchBy));
+            usrServices.add(i.id);
+          }
+          List<QueryDocumentSnapshot> filteredSearchQs = [];
+
+          aqs.then((a) {
+            for (var doc in a.docs) {
+              String vari = doc.id;
+              if (usrServices.contains(vari)) {
+                filteredSearchQs.add(doc);
+              }
+            }
+          });
+          searchqs = Future.value(MockQuerySnapshot(filteredSearchQs));
+          setState(() {
+            searchqs.then((a) {});
+          });
+        });
+        // });
+      }
+      // After all fetch operations are complete, update the UI
+    }).catchError((e) {
+      print('Error in aqs.then: $e');
+    });
+    NestedTabBar(refreshCallback: () {
+      setState(() {}); // Call setState of NestedTabBar
+    });
+  }
+}
+
+class MockQuerySnapshot implements QuerySnapshot<Object?> {
+  @override
+  final List<QueryDocumentSnapshot<Object?>> docs;
+
+  MockQuerySnapshot(this.docs);
+
+  @override
+  // TODO: implement docChanges
+  List<DocumentChange<Object?>> get docChanges => throw UnimplementedError();
+
+  @override
+  // TODO: implement metadata
+  SnapshotMetadata get metadata => throw UnimplementedError();
+
+  @override
+  // TODO: implement size
+  int get size => throw UnimplementedError();
+}
+
+Future<QuerySnapshot> fetchUserData(String userId) async {
+  QuerySnapshot qs = await Usersdao().getUserDetails(userId);
+  return qs;
 }
 
 class NestedTabBar extends StatefulWidget {
-  const NestedTabBar({super.key});
+  final Function refreshCallback;
 
+  const NestedTabBar({super.key, required this.refreshCallback});
   @override
   _NestedTabBarState createState() => _NestedTabBarState();
 }
@@ -162,6 +417,8 @@ class NestedTabBar extends StatefulWidget {
 class _NestedTabBarState extends State<NestedTabBar>
     with TickerProviderStateMixin {
   late TabController _nestedTabController;
+  // late Future<QuerySnapshot> qs;
+
   @override
   void initState() {
     super.initState();
@@ -170,6 +427,17 @@ class _NestedTabBarState extends State<NestedTabBar>
         .then((val) {
       setState(() {});
       // GlobalVariables.instance.username = widget.uname.toString();
+    });
+    if (GlobalVariables.instance.urole == 2) {
+      aqs = fetchServices();
+    } else if (GlobalVariables.instance.urole == 1) {
+      aqs = fetchJobProfiles();
+    }
+    searchqs = aqs;
+    aqs.then((QuerySnapshot s) {
+      for (var i in s.docs) {
+        print(i.get("userid"));
+      }
     });
     _nestedTabController = TabController(length: 1, vsync: this);
   }
@@ -180,9 +448,19 @@ class _NestedTabBarState extends State<NestedTabBar>
     _nestedTabController.dispose();
   }
 
+  void refresh() {
+    widget.refreshCallback();
+  }
+
   Future<QuerySnapshot> fetchServices() async {
     String userID = FirebaseAuth.instance.currentUser!.uid;
+    // QuerySnapshot? tempqs;
     QuerySnapshot qs = await maidDao().getAllServices(userID);
+    // tempqs = qs;
+    // for (var i in qs.docs) {
+
+    // }
+    // qs.forEach((doc) => {});
     return qs;
   }
 
@@ -192,9 +470,11 @@ class _NestedTabBarState extends State<NestedTabBar>
     return qs;
   }
 
-  Future<QuerySnapshot> fetchUserData(String userId) async {
-    QuerySnapshot qs = await Usersdao().getUserDetails(userId);
-    return qs;
+  void _onServiceListUpdated() {
+    setState(() {
+      // You can update your state here.
+      print("Service list updated, now calling setState!");
+    });
   }
 
   getServiceDetail(item, servItem) {
@@ -880,6 +1160,7 @@ class _NestedTabBarState extends State<NestedTabBar>
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
+
     return AnimatedBuilder(
         animation: GlobalVariables.instance,
         builder: (context, child) {
@@ -924,8 +1205,9 @@ class _NestedTabBarState extends State<NestedTabBar>
                             children: <Widget>[
                               FutureBuilder(
                                   // StreamBuilder(
-                                  future: fetchServices(),
+                                  // future: fetchServices(),
                                   // stream: fetchChats(),
+                                  future: searchqs,
                                   builder: (context, snapshot) {
                                     if (snapshot.hasData) {
                                       return GridView.builder(
@@ -1109,7 +1391,7 @@ class _NestedTabBarState extends State<NestedTabBar>
                             children: <Widget>[
                               FutureBuilder(
                                   // StreamBuilder(
-                                  future: fetchJobProfiles(),
+                                  future: searchqs,
                                   // stream: fetchChats(),
                                   builder: (context, snapshot) {
                                     if (snapshot.hasData) {
